0ce8be39bdc63b5ee6f9a2aee72e4e71bdd7a2db
oscarka <ka-0724@hotmail.com>
2023-12-05 18:24:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-
 from flask import Flask, request, make_response
 import hashlib
 import xml.etree.ElementTree as ET
+import base64
+from Crypto.Cipher import AES
+import xmltodict
+import json
+import time
 
 app = Flask(__name__)
 
-# 这里替换成您自己的Token
+# 替换成您自己的Token和EncodingAESKey
 WECHAT_TOKEN = 'oscar'
+WECHAT_ENCODING_AES_KEY = 'ekrWcwnkMHXqmAr1TxjfmcS7GeYsqUdGS8WXclTrRxH'
+WECHAT_APPID = 'wx913eb91e3e41654e'
+
+class PrpCrypt(object):
+    def __init__(self, key):
+        self.key = base64.b64decode(key + '=')
+        self.mode = AES.MODE_CBC
+
+    def decrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, self.key[:16])
+        plain_text = cryptor.decrypt(base64.b64decode(text))
+        return plain_text.rstrip(b'\0')
+
+def decrypt_msg(msg, timestamp, nonce):
+    pc = PrpCrypt(WECHAT_ENCODING_AES_KEY)
+    xml_content = pc.decrypt(msg)[20:].decode('utf-8')
+    xml_content = xml_content[:xml_content.rindex('>')+1]
+    xml_dict = xmltodict.parse(xml_content)
+    return xml_dict['xml']
 
 @app.route('/wechat', methods=['GET', 'POST'])
 def wechat():
@@ -28,25 +51,38 @@ def wechat():
             return ""
     else:
         # 处理微信发来的消息
-        xml_recv = ET.fromstring(request.data)
-        msg_type = xml_recv.find("MsgType").text
-        from_user = xml_recv.find("FromUserName").text
-        to_user = xml_recv.find("ToUserName").text
+        encrypt_type = request.args.get('encrypt_type')
+        msg_signature = request.args.get('msg_signature')
+        
+        if encrypt_type == 'aes':
+            timestamp = request.args.get('timestamp')
+            nonce = request.args.get('nonce')
+            data = xmltodict.parse(request.data)
+            encrypted_msg = data['xml']['Encrypt']
 
-        if msg_type == "text":
-            reply_text = "您发送了文本消息"
-            reply_xml = f'''
-            <xml>
-            <ToUserName><![CDATA[{from_user}]]></ToUserName>
-            <FromUserName><![CDATA[{to_user}]]></FromUserName>
-            <CreateTime>{int(time.time())}</CreateTime>
-            <MsgType><![CDATA[text]]></MsgType>
-            <Content><![CDATA[{reply_text}]]></Content>
-            </xml>
-            '''
-            return reply_xml
+            # 解密消息
+            decrypted_msg = decrypt_msg(encrypted_msg, timestamp, nonce)
+            msg_type = decrypted_msg['MsgType']
+            from_user = decrypted_msg['FromUserName']
+            to_user = decrypted_msg['ToUserName']
+
+            if msg_type == "text":
+                reply_text = "您发送了文本消息"
+                reply_xml = f"""
+                <xml>
+                <ToUserName><![CDATA[{from_user}]]></ToUserName>
+                <FromUserName><![CDATA[{to_user}]]></FromUserName>
+                <CreateTime>{int(time.time())}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[{reply_text}]]></Content>
+                </xml>
+                """
+                return reply_xml
+            else:
+                return "success"
         else:
             return "success"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
