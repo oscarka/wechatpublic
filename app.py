fac7caa74ffba08534804ef2aad036a56295f7c7
oscarka <ka-0724@hotmail.com>
2023-12-06 18:26:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,22 +26,49 @@ def wechat():
         logger.info("Received GET request with params: %s", request.args)
 
         # 微信认证
-        # 微信认证逻辑应在此处实现，但此处省略以保持简洁
-        # ...
+        signature = request.args.get('signature')
+        timestamp = request.args.get('timestamp')
+        nonce = request.args.get('nonce')
+        echostr = request.args.get('echostr')
 
-        # 假定验证成功
-        echostr = request.args.get('echostr', '')
-        return make_response(echostr)
+        token = WECHAT_TOKEN
+        data = [token, timestamp, nonce]
+        data.sort()
+        temp = ''.join(data)
+        hashcode = hashlib.sha1(temp.encode('utf-8')).hexdigest()
+        
+        # 记录认证结果
+        if hashcode == signature:
+            logger.info("Verified GET request successfully.")
+            return make_response(echostr)
+        else:
+            logger.warning("Failed to verify GET request.")
+            return make_response("Verification failed", 403)
 
     else:
         # 记录POST请求的数据
         logger.info("Received POST request with data: %s", request.data)
 
         # 处理微信发来的消息
-        # 消息处理逻辑应在此处实现，但此处省略以保持简洁
-        # ...
+        xml_recv = ET.fromstring(request.data)
+        msg_type = xml_recv.find("MsgType").text
+        from_user = xml_recv.find("FromUserName").text
+        to_user = xml_recv.find("ToUserName").text
 
-        return "success"
+        if msg_type == "text":
+            reply_text = "您发送了文本消息"
+            reply_xml = f"""
+            <xml>
+            <ToUserName><![CDATA[{from_user}]]></ToUserName>
+            <FromUserName><![CDATA[{to_user}]]></FromUserName>
+            <CreateTime>{int(time.time())}</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[{reply_text}]]></Content>
+            </xml>
+            """
+            return reply_xml
+        else:
+            return "success"
 
 if __name__ == '__main__':
     app.run(debug=True)
